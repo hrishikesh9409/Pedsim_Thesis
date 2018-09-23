67f128ad9ba558728ab0501504f720216c31ed7f
Hrishikesh <hrishikesh9409@gmail.com>
2018-09-24 01:17:57 +0200

    Finally got cells to work in pedsim

--- coppito0/src/coppito.cpp
+++ coppito0/src/coppito.cpp
@@ -3,15 +3,15 @@
 double cell_length = 22.5;
 double cell_width = 15;
 
+std::vector<std::vector<vertex> > cell_division;
+
 Coppito::Coppito(){
 }
 
 void Coppito::print(std::vector<coordinates> scene_coordinates){
 	for(int i = 0; i < scene_coordinates.size(); i++){
-        std::cout << scene_coordinates[i].x1 << /*"," << scene_coordinates[i].y1 << " --> " << scene_coordinates[i].x2 << "," << scene_coordinates[i].y2 <<*/ std::endl;
+        std::cout << scene_coordinates[i].x1 << "," << scene_coordinates[i].y1 << " --> " << scene_coordinates[i].x2 << "," << scene_coordinates[i].y2 << std::endl;
     }
-
-	//std::cout << cell_width << "    " << cell_length << std::endl;
 }
 
 void Coppito::divide_Cells(std::vector<coordinates> scene_coordinates){
@@ -33,65 +33,20 @@ void Coppito::divide_Cells(std::vector<coordinates> scene_coordinates){
 		cell_coordinate.push_back(v[2]); 
 		cell_coordinate.push_back(v[3]); 
 
-		if(i == 2){
-			for(int j = 0; j < cell_coordinate.size(); j++){
-				std::cout << cell_coordinate[j].x << std::endl;
-			}
-		}
+		cell.push_back(cell_coordinate);	
 
-		// 	cell.push_back(cell_coordinate);
-		// 	std::cout << cell.size() << std::endl;
-		// 	for(int i = 0; i < cell.size(); i++){
-		// 		for(int j = 0; j < cell_coordinate.size(); j++){
-		// 			std::cout << cell[i][j].x << std::endl;
-		// 		}
-		// 	}
-		// }
-		
-		// cell.push_back(cell_coordinate[0]);
-
-		// if(i == 0){
-		// 	for(int i = 0; i < cell.size(); i++){
-		// 		for(int j = 0; j < cell_coordinate.size(); j++){
-		// 			std::cout << cell[i][j].x << std::endl;
-		// 		}
-		// 	}
-		// }
-			
+		cell_coordinate.clear();
 	}
-	
 
-	// std::cout << cell_coordinate.size() << std::endl;
-	// for(int i = 0; i < 1; i++){
-	// 	for(int j = 0; j < 4; j++){
-	// 		if(j == 0){
-	// 			Cells[i].x1 = cell_coordinate[j].x;
-	// 			Cells[i].y1 = cell_coordinate[j].y;
-	// 		}
-	// 		if(j == 1){
-	// 			Cells[i].x2 = cell_coordinate[j].x;
-	// 			Cells[i].y2 = cell_coordinate[j].y;
-	// 		}
-	// 		if(j == 2){
-	// 			Cells[i].x3 = cell_coordinate[j].x;
-	// 			Cells[i].y3 = cell_coordinate[j].y;
-	// 		}
-	// 		if(j == 3){
-	// 			Cells[i].x4 = cell_coordinate[j].x;
-	// 			Cells[i].y4 = cell_coordinate[j].y;
-	// 		}
-	// 	}
-	// }
+	std::cout << std::endl;
+	for(int i = 0; i < cell.size(); i++){
+		std::cout << "Cell : " << i << " : " << std::endl;
+		for(int j = 0; j < 4; j++){
+			std::cout << cell[i][j].x << "," << cell[i][j].y << std::endl;
+		}
+		std::cout << std::endl;
+	}
 
-	
+	cell_division = cell;
 
-
-	// std::cout << cell[0][0].x << " --> " << cell[0][3].x << std::endl;
-	// std::cout << cell[0][0].y << " --> " << cell[0][3].y << std::endl;
-
-	//std::cout << cell[4][3].x << std::endl;
-	//std::cout << scene_coordinates[1].y1 << std::endl;
-	//print(scene_coordinates);
-
-	//std::cout << cell.size() << std::endl;
 }
--- coppito0/src/coppito.h
+++ coppito0/src/coppito.h
@@ -6,32 +6,19 @@
 extern double cell_length;
 extern double cell_width;
 
+
 struct vertex{
 	double x;
 	double y;
 };
 
-// struct cell{
-// 	double x1;
-// 	double y1;
-
-// 	double x2;
-// 	double y2;
-
-// 	double x3;
-// 	double y3;
-
-// 	double x4;
-// 	double y4;
-// };
+extern std::vector<std::vector<vertex> > cell_division;
 
 class Coppito{
 private:
 	vertex v[4];
-
-	//std::vector<cell> Cells;
 	std::vector<vertex> cell_coordinate;
-	//std::vector<std::vector<vertex> > cell;
+	std::vector<std::vector<vertex> > cell;
 
 public:
 	Coppito();
