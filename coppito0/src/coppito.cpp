c83bba0a6dd0f404f0a9be409217ce5501cc5dd4
Hrishikesh <hrishikesh9409@gmail.com>
2018-09-25 21:04:52 +0200

    partial cell division

--- coppito0/src/coppito.cpp
+++ coppito0/src/coppito.cpp
@@ -17,7 +17,7 @@ void Coppito::print(std::vector<coordinates> scene_coordinates){
 void Coppito::divide_Cells(){
 
 	start.x = -120;
-	start.y = -90;
+	start.y = -90.0;
 
 	temp = start;
 
@@ -27,7 +27,7 @@ void Coppito::divide_Cells(){
 	end[1].x = -7.5;
 	end[1].y = -60.0;
 
-	int i = 0;
+	//int i = 0;
 	while(true){
 		cs[0].x = start.x;
 		cs[0].y = start.y;
@@ -42,6 +42,60 @@ void Coppito::divide_Cells(){
 		cs[3].y = start.y + cell_width;
 
 		if((temp.x == -120.0 && temp.y == -90.0) && (start.x == -120.0 && start.y == -90.0)){
+			iterator_vertices[1].x = cs[3].x;
+			iterator_vertices[1].y = cs[3].y;
+		}
+
+		edge_vertices.push_back(cs[0]);
+		edge_vertices.push_back(cs[1]);
+		edge_vertices.push_back(cs[2]);
+		edge_vertices.push_back(cs[3]);
+
+		cell.push_back(edge_vertices);
+		edge_vertices.clear();
+
+		iterator_vertices[0].x = cs[1].x;
+		iterator_vertices[0].y = cs[1].y;
+
+		start = iterator_vertices[0];
+
+		if(cs[1].x == end[0].x){
+			start = iterator_vertices[1];		
+		}
+		if(cs[2].x == end[1].x && cs[2].y == end[1].y){
+			break;
+		}
+		//i++;
+	}
+
+	edge_vertices.clear();
+
+	start.x = 30.0;
+	start.y = -90.0;
+
+	temp = start;
+
+	end[0].x = 120.0;
+	end[0].y = -90.0;
+
+	end[1].x = 120.0;
+	end[1].y = -60.0;
+
+	//int i = 0;
+	while(true){
+		cs[0].x = start.x;
+		cs[0].y = start.y;
+
+		cs[1].x = start.x + cell_length;
+		cs[1].y = start.y;
+
+		cs[2].x = start.x + cell_length;
+		cs[2].y = start.y + cell_width;
+
+		cs[3].x = start.x;
+		cs[3].y = start.y + cell_width;
+
+		if((temp.x == 30.0 && temp.y == -90.0) && (start.x == 30.0 && start.y == -90.0)){
 			iterator_vertices[1].x = cs[3].x;
 			iterator_vertices[1].y = cs[3].y;
 		}
